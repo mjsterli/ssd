cc61e81fa56f7870813203b7fd8f6cad9f17fee1
Curtis C. Love <curtis.c.love@gmail.com>
2025-01-13 22:44:38 -0600

    adding db query for returning customer

--- src/server/twilio/handlers/greeting.ts
+++ src/server/twilio/handlers/greeting.ts
@@ -1,11 +1,30 @@
+import prisma from '../../db';
 import { twiml } from 'twilio';
 
 export const smsReplyGreeting = async (req, res) => {
   const smsResponse = new twiml.MessagingResponse();
 
-  smsResponse.message(`The number sending: ${req.body.From}`);
-  req
+  let customer = await getCustomerByPhoneNumber(req.body.From.slice(2));
+  smsResponse.message(customer
+                        ? `Welcome back ${customer.FirstName} ${customer.LastName}`
+                        : "Welcome new customer"
+  );
+
   res
     .type('text/xml')
     .send(smsResponse.toString());
+};
+
+const getCustomerByPhoneNumber = async (phoneNumber) => {
+  const customer = await prisma.customer.findFirst({
+    where: {
+      PhoneNumber: phoneNumber
+    },
+    select: {
+      FirstName: true,
+      LastName: true
+    }
+  });
+
+  return customer;
 }
