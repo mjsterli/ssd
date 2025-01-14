c2a5efd0ce462fd6866e2af394dc6df4dbd8de57
Curtis C. Love <curtis.c.love@gmail.com>
2025-01-13 22:17:42 -0600

    getting params from request

--- src/server/twilio/handlers/greeting.ts
+++ src/server/twilio/handlers/greeting.ts
@@ -3,8 +3,8 @@ import { twiml } from 'twilio';
 export const smsReplyGreeting = async (req, res) => {
   const smsResponse = new twiml.MessagingResponse();
 
-  smsResponse.message("Welcome to Simple Sign Delivery automated ordering system for sign pick-up and delivery.");
-  
+  smsResponse.message(`The number sending: ${req.body.From}`);
+  req
   res
     .type('text/xml')
     .send(smsResponse.toString());
